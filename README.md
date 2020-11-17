290fa7fe7d689c3eb7c6d1564e4f7dfb2a390d50
Юрий <ura280@mail.ru>
2020-11-17 10:23:28 +0300

    a little bit of refactoring

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # FlickrAngular
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 8.3.18.
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 11.0.0.
 
 ## Development server
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,57 +8,50 @@ import * as _        from 'underscore';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  searchVal : string;
   busy      : Promise<any>;
-  link      : any;
-  serachST  : any;
-  flickr    : any;
-  photos    : any;
-  lines     : any;
-  resolves  : any;
+  flickr    : Flickr;
+  search    : any;
 
   constructor() {
-    this.searchVal = '';
-    this.serachST  = null;
-    this.photos    = [];
-    this.lines     = 0;
-    this.busy      = null;
-    this.resolves  = [];
-    this.flickr    = Flickr('87ca6d68325684d7ed3c92331adcea37');
-    //this.flickr = Flickr.tokenOnly({
-    //  api_key: "87ca6d68325684d7ed3c92331adcea37"
-    //});
+    this.busy   = null;
+    this.flickr = Flickr('87ca6d68325684d7ed3c92331adcea37');
+    this.search = {
+      value    : '',
+      photos   : [],
+      resolves : [],
+      timeOut  : null,
+    };
   }
 
   changeSearch(){
-    clearTimeout(this.serachST);
+    clearTimeout(this.search.timeOut);
 
     this.busy = new Promise((resolve) => {
-      this.resolves.push(resolve);
+      this.search.resolves.push(resolve);
 
-      this.serachST = setTimeout(() => {
-        this.flickr.photos.search({text: this.searchVal}).then((res) => {
+      this.search.timeOut = setTimeout(() => {
+        this.flickr.photos.search({text: this.search.value}).then((res) => {
 
           res.body.photos.photo = _.map(res.body.photos.photo, p => {
             p['link'] = `https://live.staticflickr.com/${p.server}/${p.id}_${p.secret}_w.jpg`;
             return p;
           });
 
-          let i        = 0;
-          this.photos  = [];
-          let arrayRow = [];
+          this.search.photos  = [];
+          let i               = 0;
+          let arrayRow        = [];
           _.each(res.body.photos.photo, p => {
             arrayRow.push(p);
             i = i + 1;
 
             if(i == 4){
-              this.photos.push(arrayRow);
+              this.search.photos.push(arrayRow);
               i        = 0;
               arrayRow = [];
             }
           });
 
-          _.each(this.resolves, r => r({success: true}));
+          _.each(this.search.resolves, r => r({success: true}));
         }).catch(function (err) {
           console.error('search', err);
         });
